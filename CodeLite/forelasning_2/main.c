5ba7f967e44ab50227800226cc40d5698ea15ed6
Zumpo <carlsson889@gmail.com>
2017-11-09 17:53:24 +0100

    Add movement of ship

--- CodeLite/forelasning_2/main.c
+++ CodeLite/forelasning_2/main.c
@@ -4,23 +4,37 @@
 
 GfxObject ship;
 GfxObject background;
+void close(); 
+
+
+// Screen variables
+int gameWidth = 800;
+int gameHeight = 600;
+
+// Background variables
 double backgroundAngle = 0;
 double backgroundZoom = 0;
 
-void close(); 
-
 int main( int argc, char* args[] )
 {
+    // Player position
+    float playerX = 400;
+    float playerY = 300;
+    float playerSpeed = 3;
+
     // If you want the program to not launch the terminal, then go to 
     // Project->Settings->General->"This program is a GUI application" and uncheck that flag.
     
     // Start up SDL and create window of width=800, height = 600
-    initRenderer(800, 600); 
+    initRenderer(gameWidth, gameHeight); 
+    
+    // get pointer to key states
+    const Uint8 *state = SDL_GetKeyboardState(NULL); 
 
     // Create background
     background  = createGfxObject("../background.jpg");
-    background.outputWidth = 800;
-    background.outputHeight = 600;
+    background.outputWidth = gameWidth;
+    background.outputHeight = gameHeight;
     
     // Create an object
     ship = createGfxObject(  "../ship.png" );
@@ -39,18 +53,33 @@ int main( int argc, char* args[] )
             }
         }
         
+        // Handle keyboard events
+        if (state[SDL_SCANCODE_RIGHT]) {
+            playerX = (playerX + playerSpeed >= gameWidth) ? gameWidth : playerX + playerSpeed;
+        }
+        if(state[SDL_SCANCODE_LEFT]) {
+            playerX = (playerX - playerSpeed <= 0) ? 0 : playerX - playerSpeed;
+        }
+        if(state[SDL_SCANCODE_UP]) {
+            playerY = (playerY - playerSpeed <= 0) ? 0 : playerY - playerSpeed;
+        }
+        if(state[SDL_SCANCODE_DOWN]) {
+            playerY = (playerY + playerSpeed >= gameHeight) ? gameHeight : playerY + playerSpeed;
+        }
+        
         // Clear screen with a grey background color, red=0x33, blue=0x33, green=0x33, alpha=0xff. 0=minimum, 0xff=maximum.
         // Alpha is transparency: 0 = fully transparent, 0xFF = fully opaque. However, actual use of transparency requires further settings.
         SDL_SetRenderDrawColor( gRenderer, 0x33, 0x33, 0x33, 0xFF ); 
         SDL_RenderClear( gRenderer );
         renderBackground();
         // Render our object(s) - background objects first, and then forward objects (like a painter)
-        renderGfxObject(&ship, 400, 300, 0, 1.0f);
+        renderGfxObject(&ship, playerX, playerY, 0, 1.0f);
         renderText("Hello World!", 300, 150);
          
         // This function updates the screen and also sleeps ~16 ms or so (based on the screen's refresh rate),  
         // because we used the flag SDL_RENDERER_PRESENTVSYNC in function initRenderer()
         SDL_RenderPresent( gRenderer );
+
     }
 
 	close(); //Free allocated resources
@@ -58,7 +87,7 @@ int main( int argc, char* args[] )
 }
 
 void renderBackground() {
-    renderGfxObject(&background, 400, 300, fmod(backgroundAngle, 360), (1.8f + 0.2*sin(backgroundZoom)));
+    renderGfxObject(&background, gameWidth/2, gameHeight/2, fmod(backgroundAngle, 360), (1.8f + 0.2*sin(backgroundZoom)));
     backgroundAngle += 0.03;
     backgroundZoom += 0.007;
 }
