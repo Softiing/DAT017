718275d7e004b7a49bb1f7fc2c1630b9b7c3913f
Zumpo <carlsson889@gmail.com>
2017-11-21 10:46:35 +0100

    Add update and render functions to GameObject

--- CodeLite/forelasning_2/gameobject.h
+++ CodeLite/forelasning_2/gameobject.h
@@ -1,12 +1,14 @@
 #ifndef GAMEOBJECT_H
 #define GAMEOBJECT_H
 
-typedef struct {
+typedef struct tGameObject{
 	GfxObject gfxObject;
 	vec2f position;
 	float speed;
 	double angle, angleSpeed;
 	float scale, scaleSpeed;
+	void (*update) (struct tGameObject* this);
+	void (*render) (struct tGameObject* this);
 } GameObject;
 
 #endif
--- CodeLite/forelasning_2/main.c
+++ CodeLite/forelasning_2/main.c
@@ -13,6 +13,8 @@ void close();
 int gameWidth = 800;
 int gameHeight = 600;
 
+const Uint8 *state;
+
 int t = 0;
 bool bShake = false;
 int shakeStop = 0;
@@ -45,12 +47,42 @@ void printToConsole(char* str, int x, int y) {
 	printf("%s\n", str);
 }
 
+void render(GameObject* this) {
+	renderGfxObject(&this->gfxObject, this->position.x, this->position.y, this->angle, this->scale);
+}
+
+void updatePlayer(GameObject* this) {
+	shake(&this->position);
+	// Handle keyboard events
+	if (state[SDL_SCANCODE_RIGHT]) {
+		this->position.x = (this->position.x + this->speed >= gameWidth) ? gameWidth : this->position.x + this->speed;
+	}
+	if(state[SDL_SCANCODE_LEFT]) {
+		this->position.x = (this->position.x - this->speed <= 0) ? 0 : this->position.x - this->speed;
+	}
+	if(state[SDL_SCANCODE_UP]) {
+		this->position.y = (this->position.y - this->speed <= 0) ? 0 : this->position.y - this->speed;
+	}
+	if(state[SDL_SCANCODE_DOWN]) {
+		this->position.y = (this->position.y + this->speed >= gameHeight) ? gameHeight : this->position.y + this->speed;
+	}
+}
+
+void renderBackground(GameObject* this) {
+    renderGfxObject(&this->gfxObject, gameWidth/2, gameHeight/2, this->angle, (1.8f + 0.2*sin(this->scale)));
+}
+
+void updateBackground(GameObject* this) {
+	this->angle = fmod(this->angle + this->angleSpeed, 360);
+    background.scale += background.scaleSpeed;
+}
+
 int main( int argc, char* args[] ) {
     // Start up SDL and create window of width=800, height = 600
     initRenderer(gameWidth, gameHeight); 
     
     // get pointer to key states
-    const Uint8 *state = SDL_GetKeyboardState(NULL); 
+    state = SDL_GetKeyboardState(NULL); 
 
     // Create background
     background.gfxObject = createGfxObject("../background.jpg");
@@ -58,13 +90,21 @@ int main( int argc, char* args[] ) {
     background.gfxObject.outputHeight = gameHeight;
 	background.angleSpeed = 0.03;
 	background.scaleSpeed = 0.007;
+	background.update = updateBackground;
+	background.render = renderBackground;
     
 	// Player ship init
 	player.position = (vec2f){400, 300};
 	player.speed = 3;
+	player.scale = 1;
     player.gfxObject = createGfxObject(  "../ship.png" );
     player.gfxObject.outputWidth  = 200;
     player.gfxObject.outputHeight = 200;
+	player.update = updatePlayer;
+	player.render = render;
+	
+	// Create array of gameobjects
+	GameObject gameObjects[] = {background, player};
 	
 	// Init print function pointer
 	void (*print) (char* str, int x, int y) = printToWindow;
@@ -88,29 +128,23 @@ int main( int argc, char* args[] ) {
 					}
 			}
         }
-        
-		shake(&player.position);
-        // Handle keyboard events
-        if (state[SDL_SCANCODE_RIGHT]) {
-            player.position.x = (player.position.x + player.speed >= gameWidth) ? gameWidth : player.position.x + player.speed;
-        }
-        if(state[SDL_SCANCODE_LEFT]) {
-            player.position.x = (player.position.x - player.speed <= 0) ? 0 : player.position.x - player.speed;
-        }
-        if(state[SDL_SCANCODE_UP]) {
-            player.position.y = (player.position.y - player.speed <= 0) ? 0 : player.position.y - player.speed;
-        }
-        if(state[SDL_SCANCODE_DOWN]) {
-            player.position.y = (player.position.y + player.speed >= gameHeight) ? gameHeight : player.position.y + player.speed;
-        }
-        
+                
         // Clear screen with a grey background color, red=0x33, blue=0x33, green=0x33, alpha=0xff. 0=minimum, 0xff=maximum.
         // Alpha is transparency: 0 = fully transparent, 0xFF = fully opaque. However, actual use of transparency requires further settings.
         SDL_SetRenderDrawColor( gRenderer, 0x33, 0x33, 0x33, 0xFF ); 
         SDL_RenderClear( gRenderer );
-        renderBackground();
-        // Render our object(s) - background objects first, and then forward objects (like a painter)
-        renderGfxObject(&player.gfxObject, player.position.x, player.position.y, 0, 1.0f);
+		
+		// Update all gameobjects
+		for(int i = 0; i < (sizeof(gameObjects)/sizeof(*gameObjects)); i++) {
+			gameObjects[i].update(&gameObjects[i]);
+		}
+		
+		// Render all gameobjects
+		for(int i = 0; i < (sizeof(gameObjects)/sizeof(*gameObjects)); i++) {
+			gameObjects[i].render(&gameObjects[i]);
+		}
+
+
         print("Hello World!", 300, 150);
          
         // This function updates the screen and also sleeps ~16 ms or so (based on the screen's refresh rate),  
@@ -121,12 +155,6 @@ int main( int argc, char* args[] ) {
 
 	close(); //Free allocated resources
 	return 0;
-}
-
-void renderBackground() {
-    renderGfxObject(&background.gfxObject, gameWidth/2, gameHeight/2, fmod(background.angle, 360), (1.8f + 0.2*sin(background.scale)));
-    background.angle += background.angleSpeed;
-    background.scale += background.scaleSpeed;
 }
 
 void close()
